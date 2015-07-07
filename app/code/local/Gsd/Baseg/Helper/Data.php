be18b772dc5d246a94c8f0ecc401f19eeb5416d1
giangsoda <giangsoda@gmail.com>
2015-07-07 16:33:19 +0700

    baseg helper

--- app/code/local/Gsd/Baseg/Helper/Data.php
+++ app/code/local/Gsd/Baseg/Helper/Data.php
@@ -1,5 +1,16 @@
 <?php
 
 class Gsd_Baseg_Helper_Data extends Mage_Core_Helper_Abstract {
-    
+    public function translateCode($code)
+    {
+        $processor = Mage::getSingleton('core/email_template_filter');
+        return $processor->filter($code);
+    }
+    public function getReviewHtml($product)
+    {
+        if(!$product->getId()) {
+            return null;
+        }
+        return Mage::app()->getLayout()->createBlock('review/helper')->getSummaryHtml($product, false,false);;
+        //$this->getReviewsSummaryHtml($product) //block Mage_Catalog_Block_Product_Abstract
 }
