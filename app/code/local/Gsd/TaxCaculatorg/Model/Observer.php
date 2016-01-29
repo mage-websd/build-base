03a14977467bdc7f1e67f1c0ed54c55f8e2baf3a
giangsoda <giangsoda@gmail.com>
2016-01-30 00:31:36 +0700

    push wp plugin

--- app/code/local/Gsd/TaxCaculatorg/Model/Observer.php
+++ app/code/local/Gsd/TaxCaculatorg/Model/Observer.php
@@ -1,17 +1,31 @@
 <?php
 class Gsd_TaxCaculatorg_Model_Observer extends Mage_Sales_Model_Quote_Address_Total_Abstract{
     public function calculatePartialPrice($observer) {
+        if(Mage::app()->getRequest()->isXmlHttpRequest()) {
+            return;
+        }
         $quote = $observer->getEvent()->getQuote();
-
+        if(!$quote) {
+            return;
+        }
+        $totals = $quote->getTotals();
+        if(!$totals) {
+            return;
+        }
+        $quote->setGrandTotal(10);
+        $quote->setBaseGrandTotal(10);
+        $quote->save();
+        return;
         $grandTotal = $quote->getGrandTotal();
         $baseGrandTotal = $quote->getBaseGrandTotal();
-
-        $totals = $quote->getTotals();
-
+        if(isset($totals["discount"]) && $totals["discount"]) {
+            $discount = $totals["discount"]->getValue();
+        }
+        else {
+            $discount = 0;
+        }
         $subtotal = $totals['subtotal'];
         $subtotalExclTax = $subtotal->getData('value_excl_tax');
-
-        $discount = $totals["discount"]->getValue();
 
         $grandTotalExcl = $subtotalExclTax - abs((float)$discount);
         $tax = $grandTotalExcl * 0.06;
@@ -35,15 +49,51 @@ class Gsd_TaxCaculatorg_Model_Observer extends Mage_Sales_Model_Quote_Address_To
         exit;*/
 
 
-        $quote->setGrandTotal(20);
-        $quote->setBaseGrandTotal(20);
+        /*$quote->setGrandTotal(20);
+        $quote->setBaseGrandTotal(20);*/
 
         $quote->setSubtotal(10);
         $quote->setBaseSubtotal(10);
 
         /*$quote->setSubtotalWithDiscount(0);
         $quote->setBaseSubtotalWithDiscount(0);*/
-        $quote->collectTotals()->save();
+        //$quote->collectTotals()->save();
         $quote->save();
+    }
+
+    public function salesQuoteCollectTotalsBefore($observer) {
+        return;
+        if(Mage::app()->getRequest()->isXmlHttpRequest()) {
+            return;
+        }
+        $quote = $observer->getEvent()->getQuote();
+        if(!$quote) {
+            return;
+        }
+        $totals = $quote->getTotals();
+        if(!$totals) {
+            return;
+        }
+        $quote->setSubtotal(10);
+        $quote->setBaseSubtotal(10);
+        //$quote->setTotalsCollectedFlag(false)->collectTotals();
+        $quote->save();
+    }
+
+    public function salesQuoteSaveBefore($observer) {
+        return;
+        $quote = $observer->getEvent()->getQuote();
+        if(!$quote) {
+            return;
+        }
+        $totals = $quote->getTotals();
+        if(!$totals) {
+            return;
+        }
+        $quote->setSubtotal(10);
+        $quote->setBaseSubtotal(10);
+        $quote->setTotalsCollectedFlag(false)->collectTotals();
+        session_write_close();
+        //$quote->save();
     }
 }
