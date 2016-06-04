fc4ee5a41ea79d37a27a4d0aadacef8e891ab757
giangnt2 <giangnt2@rikkeisoft.com>
2016-06-05 00:15:31 +0700

    a

--- app/code/local/Gsd/Baseg/Model/Base.php
+++ app/code/local/Gsd/Baseg/Model/Base.php
@@ -1,24 +1,25 @@
 <?php
+
 class Gsd_Baseg_Model_Base {
-    
+
     //add store, column in db format: 0,1,2,3,4,5
-    public function addStoresFilter($store=null) {
+    public function addStoresFilter($store = null) {
         if ($store === null) {
             $store = Mage::app()->getStore();
         }
-        if(is_numeric($store)) {
+        if (is_numeric($store)) {
             $storeId = $store;
             $store = Mage::app()->getStore($store);
-        }
-        else {
+        } else {
             $storeId = $store->getId();
         }
-        if($store->isAdmin()) {
+        if ($store->isAdmin()) {
             return $this;
         }
         $storeAdminId = Mage_Core_Model_App::ADMIN_STORE_ID;
-        $this->getSelect()->where("find_in_set('{$storeId}',main_table.stores) OR ".
+        $this->getSelect()->where("find_in_set('{$storeId}',main_table.stores) OR " .
                 "find_in_set('{$storeAdminId}',main_table.stores)");
         return $this;
     }
+
 }
